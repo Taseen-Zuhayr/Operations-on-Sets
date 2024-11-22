ecd49c11a6e8da62e3b97924ef2109706b22ea55
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-22 22:28:29 +0600

    set

---
+++ main.py
@@ -0,0 +1,15 @@
+x = {24,2,4,24,24,45}
+print(x)
+
+y = {"codingal", True, 3,4.5}
+print(y)
+
+z = set([1,2,3,2])
+print(z)
+
+n = set([0,1,7,3,4])
+print("before removing:",n)
+n.pop()
+print("after removing",n)
+n.remove(2)
+print("removing number 7:",n)
